5c5a9d4300ad498f25cdec9ef8f418ef585f29d9
jhaaaannn <mechriahana@gmail.com>
2023-01-11 10:25:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Formulaire from './component/Formulaire';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Form
+        </p>
+        
+      </header>
+      <Formulaire />
+    </div>
+  );
+}
+
+export default App;
